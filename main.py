fb210a6e3416a672948ac260237fbe8d8401b4d6
Carl Lemaire <c.lemaire@live.ca>
2022-07-06 23:36:36 -0400

    Listen for ping, send ping in response

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ from pythonosc import udp_client
 import asyncio
 
 
-def pong(address, *args):
-    print(f"received {address}: {args}")
+def on_receive_ping(ip_address_and_port, address, *args):
+    print(f"received ping")
+    
+    # Configure a temporary client to send the pong
+    ip_address, port = ip_address_and_port
+    client = udp_client.SimpleUDPClient(ip_address, port)
+    client.send_message("/pong", 7000)
+    
+def on_receive_pong(ip_address, address, *args):
+    print(f"received pong")
 
 # Send to this address
-client = udp_client.SimpleUDPClient("127.0.0.1", 1337)
+client = udp_client.SimpleUDPClient("192.168.1.94", 54321)
 
 dispatcher = Dispatcher()
-dispatcher.map("/ping", pong)
+dispatcher.map("/ping", on_receive_ping, needs_reply_address=True)
+dispatcher.map("/pong", on_receive_pong, needs_reply_address=True)
 
 # For receiving
-ip = "127.0.0.1"
-port = 1337
+ip = "192.168.1.103"
+port = 7000
 
 
 async def loop():
