5d6b10f86ed098e27ec68ebde7b03abffb016bd1
Carl Lemaire <c.lemaire@live.ca>
2022-07-13 21:07:33 -0400

    Store state; transform; send at fixed rate

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def on_receive_ping(ip_address_and_port, address, *args):
     
 def on_receive_pong(ip_address, address, *args):
     print(f"received pong")
+    
+def on_receive_input(address, *args):
+    key = address[1:]
+    input_state[key] = args[0]
 
 # Send to this address
 client = udp_client.SimpleUDPClient("192.168.1.94", 54321)
@@ -25,17 +29,31 @@ client = udp_client.SimpleUDPClient("192.168.1.94", 54321)
 dispatcher = Dispatcher()
 dispatcher.map("/ping", on_receive_ping, needs_reply_address=True)
 dispatcher.map("/pong", on_receive_pong, needs_reply_address=True)
+dispatcher.map("/i", on_receive_input)
 
 # For receiving
 ip = "192.168.1.103"
 port = 7000
 
+input_state = {'test': 0}
+
+
+def transform(state):
+    # TODO transform state by creating new keys here
+    # Just a dummy transformation for now
+    return {"out_" + k: v for k, v in state.items()}
+
+
+def send_all(state):
+    for k, v in state.items():
+        client.send_message('/' + k, v)
+
 
 async def loop():
-    """Example main loop that only runs for 10 iterations before finishing"""
-    for i in range(10):
-        print(f"{i}: sending ping")
-        client.send_message("/ping", 0)
+    while True:
+        out_state = transform(input_state)
+        print(out_state)
+        send_all(out_state)
         await asyncio.sleep(1)
 
 
